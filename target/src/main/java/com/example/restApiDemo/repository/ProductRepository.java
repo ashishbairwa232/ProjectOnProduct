1e5eb452ff2e4325217f01ca41044a4bc9a4dff9
Ashish <ashishbairwa232@gmail.com>
2022-11-16 13:34:45 +0530

    initial commit

---
+++ target/src/main/java/com/example/restApiDemo/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.restApiDemo.repository;
+
+import com.example.restApiDemo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+}
